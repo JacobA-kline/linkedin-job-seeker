0b060f71bfde1c7b538cbae9abf6be2b0a8d2f19
Jacob Kline <kobekline@gmail.com>
2022-12-02 10:25:47 +0200

    add class JobSeekerEngine, make a method out of function get_user_input, method login_linkedin, method search_by_job_type, method search_with_experience_level, and method to click easy_apply button.

---
+++ job_seeker_engine.py
@@ -0,0 +1,102 @@
+import os
+import dotenv
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+dotenv.load_dotenv(".env")
+
+
+class JobSeekerEngine:
+    def __init__(self):
+        self.PASSWORD = os.getenv("LINKEDIN_PASS")
+        self.EMAIL = os.getenv("EMAIL")
+        self.chrome_driver_path = Service("/home/jacob/Development/chromedriver_linux64/chromedriver")
+        self.chrome_driver = webdriver.Chrome(service=self.chrome_driver_path)
+        self.chrome_driver.minimize_window()
+        self.job_type = ""
+        self.experience_level = int
+        self.wait = WebDriverWait(self.chrome_driver, 7)
+
+    def get_user_input(self):
+        """Asks user a Job Type and Experience level"""
+        checking_job_type = True
+        while checking_job_type:
+            try:
+                self.job_type = str(input("What type of job are you interested in?\n"))
+            except ValueError:
+                print("Please enter words only.")
+                continue
+            else:
+                checking_job_type = False
+        checking_experience_level = True
+        while checking_experience_level:
+            try:
+                self.experience_level = int(input(f"Please choose an experience level: \n1. Internship\n"
+                                                  f"2. Entry level\n"
+                                                  f"3. Associate\n"
+                                                  f"4. Mid-Senior level\n"))
+                if self.experience_level not in range(1, 5):
+                    print("Please enter a valid experience level 1-4.")
+                    continue
+            except ValueError:
+                print("please enter numbers only.")
+                continue
+            else:
+                checking_experience_level = False
+        return self.job_type, self.experience_level
+
+    def login_linkedin(self):
+        """Logging in LinkedIn"""
+        self.chrome_driver.get("https://www.linkedin.com/")
+        self.chrome_driver.maximize_window()
+        self.chrome_driver.find_element(By.XPATH, "/html/body/nav/div/a[2]").click()
+        time.sleep(2)
+        self.chrome_driver.find_element(By.XPATH, """//*[@id="username"]""").send_keys(self.EMAIL)
+        self.chrome_driver.find_element(By.XPATH, """//*[@id="password"]""").send_keys(self.PASSWORD)
+        self.chrome_driver.find_element(By.XPATH, """//*[@id="organic-div"]/form/div[3]/button""").click()
+        self.chrome_driver.find_element(By.XPATH, """//*[@id="global-nav"]/div/nav/ul/li[3]/a""").click()
+        time.sleep(5)
+
+    def search_by_job_type(self, job_type):
+        """Posts in the search bar the type of job"""
+        job_search_bar = self.chrome_driver.find_element(By.CSS_SELECTOR, ".relative input")
+        job_search_bar.send_keys(job_type, Keys.ENTER)
+        time.sleep(3)
+        experience_level_button = self.chrome_driver.find_element(By.XPATH, """/html/body/div[5]/div[3]/div[
+        4]/section/div/section/div/div/div/ul/li[4]/div/span/button""")
+        experience_level_button.click()
+        time.sleep(3)
+
+    def search_with_experience_level(self, experience_level):
+        """clicks on user experience level according to user output"""
+        if experience_level == 1:
+            internship = self.chrome_driver.find_element(By.XPATH, """/html/body/div[5]/div[3]/div[
+            4]/section/div/section/div/div/div/ul/li[4]/div/div/div/div[1]/div/form/fieldset/div[1]/ul/li[1]/label""")
+            internship.click()
+        elif experience_level == 2:
+            entry_level = self.chrome_driver.find_element(By.XPATH, """/html/body/div[5]/div[3]/div[
+            4]/section/div/section/div/div/div/ul/li[4]/div/div/div/div[1]/div/form/fieldset/div[1]/ul/li[2]/label""")
+            entry_level.click()
+        elif experience_level == 3:
+            associate = self.chrome_driver.find_element(By.XPATH, """/html/body/div[5]/div[3]/div[
+            4]/section/div/section/div/div/div/ul/li[4]/div/div/div/div[1]/div/form/fieldset/div[1]/ul/li[3]/label""")
+            associate.click()
+        else:
+            mid_level = self.chrome_driver.find_element(By.XPATH, """/html/body/div[5]/div[3]/div[
+            4]/section/div/section/div/div/div/ul/li[4]/div/div/div/div[1]/div/form/fieldset/div[1]/ul/li[4]/label""")
+            mid_level.click()
+
+    def easy_apply(self):
+        """Clicks on the easy apply button"""
+        easy_apply_button = self.chrome_driver.find_element(By.XPATH, """/html/body/div[5]/div[3]/div[
+        4]/section/div/section/div/div/div/ul/li[8]/div/button""")
+        easy_apply_button.click()
+        time.sleep(3)
+
+
+
--- main.py
+++ main.py
@@ -1,28 +1,9 @@
-import os
-import dotenv
-import time
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
+from job_seeker_engine import JobSeekerEngine
 
+j = JobSeekerEngine()
 
-dotenv.load_dotenv(".env")
-PASSWORD = os.getenv("LINKEDIN_PASS")
-EMAIL = os.getenv("EMAIL")
-
-chrome_driver_path = Service("/home/jacob/Development/chromedriver_linux64/chromedriver")
-chrome_driver = webdriver.Chrome(service=chrome_driver_path)
-chrome_driver.maximize_window()
-chrome_driver.get("https://www.linkedin.com/")
-
-chrome_driver.find_element(By.XPATH, "/html/body/nav/div/a[2]").click()
-time.sleep(2)
-chrome_driver.find_element(By.XPATH, """//*[@id="username"]""").send_keys(EMAIL)
-chrome_driver.find_element(By.XPATH, """//*[@id="password"]""").send_keys(PASSWORD)
-chrome_driver.find_element(By.XPATH, """//*[@id="organic-div"]/form/div[3]/button""").click()
-
-
-
-
-
+j.get_user_input()
+j.login_linkedin()
+j.search_by_job_type(job_type=j.job_type)
+j.search_with_experience_level(experience_level=j.experience_level)
+j.easy_apply()
